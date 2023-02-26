892c55a919e0b6d5c07526cdeaba80ade1c37f67
Lone <94772757+SurvivorJosh@users.noreply.github.com>
2023-02-25 20:49:34 -0500

    Update streambot.py

--- streambot.py
+++ streambot.py
@@ -1,10 +1,13 @@
-import kaneki
-import asyncio
+import os
+try:
+    from fymjosh import Client, Status
+    
+except:
+    os,system('pip install fymjosh')
 
 token = input("Token?: ")
 stream_name = input("Stream Name?: ")
 stream_url = input("Stream URL?: ")
 
-
-client = kaneki.Client(kaneki.Status.streaming(stream_name, stream_url))
+client = Client(Status.streaming(stream_name, stream_url))
 client.run(token)
